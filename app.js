18329112994c1d0eeb1621dfbafee7225bdb79aa
김채민 <dktjdej25@gmail.com>
2024-08-26 13:30:00 +0900

    config: postgresql 데이터베이스 연결 설정

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 // server.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const quoteRoutes = require('./app/routes/quoteRoutes');
+
 const app = express();
 const PORT = 3000;
+
+const quoteRoutes = require('./routes/quoteRoutes');
+
+const sequelize = require('./config/db')
 
 // 미들웨어 설정
 app.use(bodyParser.json());
@@ -11,7 +17,14 @@ app.use(bodyParser.json());
 // 라우트 설정
 app.use('/', quoteRoutes);
 
-// 서버 시작
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// 데이터베이스 동기화 및 서버 시작
+sequelize.sync({ alter: true }).then(() => {
+    console.info('Database & tables 생성');
+
+    // 기본적인 라우트 설정
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}).catch(err => {
+    console.error('Database 연결 중 오류 발생:', err);
 });
