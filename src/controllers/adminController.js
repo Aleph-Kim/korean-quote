de517e000aae59ecd64abba5893e96fe6ff98fc2
김채민 <dktjdej25@gmail.com>
2025-01-20 23:29:51 +0900

    refactor: adminController 구조 개선

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,35 +1,34 @@
 const { successResponse, errorResponse } = require('../helpers/responseHelper');
 const adminService = require('../services/adminService');
 
-/**
- * 명언 생성
- * @param {*} req 요청 객체
- * @param {*} res 응답 객체
- */
-const createQuote = async (req, res, next) => {
-    try {
-        const quote = await adminService.createQuote(req.body);
-        return successResponse(res, quote, "명언 데이터가 생성되었습니다.");
-    } catch (err) {
-        next(err)
-    }
-};
+class AdminController {
 
-/**
- * JSON 파일로 명언 생성
- * @param {*} req 요청 객체
- * @param {*} res 응답 객체
- */
-const createQuoteWithJson = async (req, res, next) => {
-    try {
-        const cnt = await adminService.createQuoteWithJson(req);
-        return successResponse(res, {}, `${cnt}개의 명언 데이터가 생성되었습니다.`);
-    } catch (err) {
-        next(err)
-    }
-};
+    /**
+     * 명언 생성
+     * @param {*} req 요청 객체
+     * @param {*} res 응답 객체
+     */
+    async createQuote(req, res, next) {
+        try {
+            const quote = await adminService.createQuote(req.body);
+            return successResponse(res, quote, "명언 데이터가 생성되었습니다.");
+        } catch (err) {
+            next(err)
+        }
+    };
 
-module.exports = {
-    createQuote,
-    createQuoteWithJson
-};
+    /**
+     * JSON 파일로 명언 생성
+     * @param {*} req 요청 객체
+     * @param {*} res 응답 객체
+     */
+    async createQuoteWithJson(req, res, next) {
+        try {
+            const cnt = await adminService.createQuoteWithJson(req);
+            return successResponse(res, {}, `${cnt}개의 명언 데이터가 생성되었습니다.`);
+        } catch (err) {
+            next(err)
+        }
+    };
+}
+module.exports = new AdminController();
